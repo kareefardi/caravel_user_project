bd74910655edfd79779de42e2b3f2a8633f13a2d
kareem <kareem.farid@efabless.com>
2021-12-29 04:52:48 -0800

    - add makefile for pico verification tests - print time while running litext verification

--- verification-pico.mk
+++ verification-pico.mk
@@ -2,30 +2,31 @@ VERIFICATION_IMAGE = efabless/dv:latest
 
 LOGS_DIR = logs
 PROJECT_ROOT=$(realpath $(PWD))
-CARAVEL_DIR = $(PROJECT_ROOT)/caravel
-CARAVEL_CORE_DIR = $(PROJECT_ROOT)/caravel_mgmt_soc_litex
-STANDALONE_DIR = $(PROJECT_ROOT)/caravel_mgmt_soc_litex/verilog/dv/tests-standalone
-CARAVEL_TEST_DIR = $(PROJECT_ROOT)/caravel_mgmt_soc_litex/verilog/dv/tests-caravel
+CARAVEL_ROOT = $(PROJECT_ROOT)/caravel
+CARAVEL_PICO_ROOT=$(PROJECT_ROOT)/caravel_pico
+PICO_STANDALONE_DIR = $(CARAVEL_PICO_ROOT)/verilog/dv/standalone
+PICO_CARAVEL_TEST_DIR = $(CARAVEL_PICO_ROOT)/verilog/dv/caravel
 
 THREADS ?= 1
 SIM ?= RTL
 
-ifeq ($(wildcard $(CARAVEL_DIR)/.),)
-$(error $(CARAVEL_DIR) not found)
+ifeq ($(wildcard $(CARAVEL_ROOT)/.),)
+$(error $(CARAVEL_ROOT) not found)
 endif
 
-ifeq ($(wildcard $(CARAVEL_CORE_DIR)/.),)
-$(error $(CARAVEL_CORE_DIR) not found)
+ifeq ($(wildcard $(CARAVEL_PICO_ROOT)/.),)
+$(error $(CARAVEL_PICO_ROOT) not found)
 endif
 
-STANDALONE = $(shell cd $(STANDALONE_DIR) && find * -maxdepth 0 -type d)
-CARAVEL_TEST = $(shell cd $(CARAVEL_TEST_DIR) && find * -maxdepth 0 -type d)
+STANDALONE = $(shell cd $(PICO_STANDALONE_DIR) && find * -maxdepth 0 -type d)
+CARAVEL_TEST = $(shell cd $(PICO_CARAVEL_TEST_DIR) && find * -maxdepth 0 -type d)
 STANDALONE_TARGETS := $(foreach i, $(STANDALONE), standalone-$(i))
 CARAVEL_TEST_TARGETS := $(foreach i, $(CARAVEL_TEST), caravel-$(i))
 
-DOCKER_ENV =  -e CARAVEL_VERILOG_PATH=$(CARAVEL_DIR)/verilog
-DOCKER_ENV += -e CORE_VERILOG_PATH=$(CARAVEL_CORE_DIR)/verilog
+DOCKER_ENV =  -e CARAVEL_PICO_ROOT=$(CARAVEL_PICO_ROOT)
+DOCKER_ENV += -e CARAVEL_ROOT=$(CARAVEL_ROOT)
 DOCKER_ENV += -e PDK_ROOT=$(PDK_ROOT)
+DOCKER_ENV += -e GCC_PREFIX=riscv32-unknown-linux-gnu
 
 check_defined = \
 		    $(strip $(foreach 1,$1, \
@@ -53,7 +54,10 @@ DOCKER_RUNNER_CMD = docker run -t \
 .PHONY: list-caravel
 .PHONY: list
 
-all: $(STANDALONE_TARGETS) $(CARAVEL_TEST_TARGETS)
+
+caravel-all : $(CARAVEL_TEST_TARGETS)
+
+all: $(STANDALONE_TARGETS) caravel-all
 
 list: list-standalone list-caravel
 
@@ -68,18 +72,18 @@ logs:
 
 $(STANDALONE_TARGETS): standalone-% : % $(LOGS_DIR)
 	@echo "Time: $$(date) --------------------------------------------------------"
-	# target: $(STANDALONE_DIR)/$*
-	# caravel: $(CARAVEL_DIR)
+	# target: $(PICO_STANDALONE_DIR)/$*
+	# caravel: $(CARAVEL_ROOT)
 	# running $@
 	@$(DOCKER_RUNNER_CMD) \
-		bash -c "source /.bashrc && env && cd $(STANDALONE_DIR)/$* && make $(SIM) -j$(THREADS)" 2>&1 > $(LOGS_DIR)/$@.log
+		bash -c "source /.bashrc && env && cd $(PICO_STANDALONE_DIR)/$* && make $(SIM) -j$(THREADS)" 2>&1 > $(LOGS_DIR)/$@.log
 	@echo "Time: $$(date) +++++++++++++++++++++++++++++++++++++++++ done $@"
 
 $(CARAVEL_TEST_TARGETS): caravel-% : % $(LOGS_DIR)
 	@echo "Time: $$(date) --------------------------------------------------------"
-	# target: $(CARAVEL_TEST_DIR)/$*
-	# caravel: $(CARAVEL_DIR)
+	# target: $(PICO_CARAVEL_TEST_DIR)/$*
+	# caravel: $(CARAVEL_ROOT)
 	# running $@
 	@$(DOCKER_RUNNER_CMD) \
-		bash -c "source /.bashrc && env && cd $(CARAVEL_TEST_DIR)/$* && make $(SIM) -j$(THREADS)" 2>&1 > $(LOGS_DIR)/$@.log
+		bash -c "source /.bashrc && env && cd $(PICO_CARAVEL_TEST_DIR)/$* && make $(SIM) -j$(THREADS)" 2>&1 > $(LOGS_DIR)/$@.log
 	@echo "Time: $$(date) +++++++++++++++++++++++++++++++++++++++++ done $@"
